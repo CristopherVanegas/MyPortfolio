ef26984da2bd009c6162556427110609a6ee4077
= <vvaleria26@gmail.com>
2024-12-22 17:39:34 -0500

    Navbar contact

--- hi-im-cristopher/src/components/navbar/Navbar.jsx
+++ hi-im-cristopher/src/components/navbar/Navbar.jsx
@@ -4,19 +4,35 @@ import styles from './Navbar.module.css'
 import { getImageUrl } from '../../utils'
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
         <a href="/" className={styles.title}>Portfolio</a>
         <div className={styles.menu}>
             <img 
                 className={styles.menuBtn} 
-                src={getImageUrl("nav/menu")} 
-                alt="menu-button" />
-            <ul className={styles.menuItems}>
-                <li><a href="">About</a></li>
-                <li><a href="">Experience</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Contact</a></li>
+                // src={ 
+                //     menuOpen
+                //       ? getImageUrl("nav/closeIcon.png")
+                //       : getImageUrl("nav/menuIcon.png")
+                // }
+                src={ 
+                    menuOpen
+                      ? "/assets/nav/closeIcon.png"
+                      : "/assets/nav/menuIcon.png"
+                }
+                onClick={() => setMenuOpen(!menuOpen)}
+                alt={
+                    menuOpen
+                    ? "close-menu-button"
+                    : "menu-button"
+                } />
+            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick={() => setMenuOpen(false)} >
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </div>
     </nav>
--- hi-im-cristopher/src/utils.js
+++ hi-im-cristopher/src/utils.js
@@ -1,4 +1,5 @@
 // para importar de forma dinamica las imagenes en la carpeta assets desde internet
 export const getImageUrl = (path) => {
     return new URL(`/assets/${path}`, import.meta.url).href;
-}
+};
+  
