466e29603ba7510555e59097ec5f9d6fbb561895
= <vvaleria26@gmail.com>
2024-12-22 21:42:23 -0500

    About component done

--- hi-im-cristopher/src/components/About/About.jsx
+++ hi-im-cristopher/src/components/About/About.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import { getImageUrl } from '../../utils'
+import React from 'react';
+
+import styles from './About.module.css'; 
+import { getImageUrl } from '../../utils';
 
 export const About = () => {
   return (
-    <section>
-        <h2>Acerca de mi</h2>
-        <div><img src={getImageUrl("about/aboutImage.png")} alt="Yo programando" /></div>
-        <ul>
-            <li><img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" /></li>
-        </ul>
+    <section className={styles.container} id='about'>
+        <h2 className={styles.title}>Acerca de mi</h2>
+        <div className={styles.content}>
+            <img src={getImageUrl("about/yo_programando.png")} alt="Yo programando" className={styles.aboutImage} />
+            <ul className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
+                    <div className={styles.aboutItemText}>
+                        <h3>Frontend Developer</h3>
+                        <p>Experiencia en creación de páginas responsive y SEO optimizado.</p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
+                    <div className={styles.aboutItemText}>
+                        <h3>Backend developer</h3>
+                        <p>Experiencia en creación de APIs optimizadas con .NET Core y manejo de bases de datos SQL y NoSQL.</p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cursorIcon.png")} alt="UI Icon" />
+                    <div className={styles.aboutItemText}>
+                        <h3>Diseño UI</h3>
+                        <p>Manejo de Figma para diseño de páginas web.</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </section>
   )
 }
