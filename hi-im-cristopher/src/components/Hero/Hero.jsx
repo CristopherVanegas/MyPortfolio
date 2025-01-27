32259e0ded6d24db7b48a56bba52f245885694af
Cristopher Vanegas <cristopher.platzi@gmail.com>
2025-01-27 01:17:00 -0500

    Se modificó la experiencia

--- hi-im-cristopher/src/components/Hero/Hero.jsx
+++ hi-im-cristopher/src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
     <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hola, soy Cristopher</h1>
-            <p className={styles.description}>Soy un desarrollador full stack con 2 años de experiencia en .NET y SQL Server.</p>
+            <p className={styles.description}>Soy un desarrollador full stack con 3 años de experiencia en .NET y SQL Server.</p>
             <a href="mailto:cristopher.vanegas.21@gmail.com" className={styles.contactBtn}>Contáctame</a>
         </div>
         <img src={ getImageUrl("hero/me_hero.jpg") } className={styles.heroImg} alt="Hero Image of me" />
