50bc0fd79214a86d931c9ec6781ce1211a52760d
Cristopher Vanegas <cristophervanegas@Cristophers-MacBook-Pro.local>
2025-07-07 13:25:44 -0500

    slider v1

--- hi-im-cristopher/src/components/Projects/Projects.jsx
+++ hi-im-cristopher/src/components/Projects/Projects.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-import styles from './Projects.module.css';
+import styles from "./Projects.module.css";
 
-import projects from '../../data/projects.json';
-import { ProjectCard } from './ProjectCard/ProjectCard';
+import projects from "../../data/projects.json";
+import { ProjectCard } from "./ProjectCard/ProjectCard";
 
 export const Projects = () => {
+  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
+
+  const nextProject = () => {
+    setCurrentProjectIndex((prevIndex) =>
+      prevIndex === projects.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevProject = () => {
+    setCurrentProjectIndex((prevIndex) =>
+      prevIndex === 0 ? projects.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
-    <section className={styles.container} id='projects'>
-        <h2 className={styles.title}>Proyectos</h2>
-        <div className={styles.projects}>{
-            projects.map((project, id) => {
-                return <ProjectCard key={id} project={project} />
-            })
-        }</div>
+    <section className={styles.container} id="projects">
+      <h2 className={styles.title}>Proyectos</h2>
+      <div className={styles.projects}>
+        <button className={styles.navButton} onClick={prevProject}>
+          &lt;
+        </button>
+        <div className={styles.projectCard}>
+          <ProjectCard project={projects[currentProjectIndex]} />
+        </div>
+        <button className={styles.navButton} onClick={nextProject}>
+          &gt;
+        </button>
+      </div>
     </section>
-  )
-}
+  );
+};
