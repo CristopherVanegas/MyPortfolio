debc89d3279263bc41011a8d7dd0daa164b0f858
Cristopher Vanegas <cristophervanegas@Cristophers-MacBook-Pro.local>
2025-07-07 15:03:21 -0500

    changed picture and about

--- hi-im-cristopher/src/components/About/About.jsx
+++ hi-im-cristopher/src/components/About/About.jsx
@@ -1,38 +1,52 @@
-import React from 'react';
+import React from "react";
 
-import styles from './About.module.css'; 
-import { getImageUrl } from '../../utils';
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
 
 export const About = () => {
   return (
-    <section className={styles.container} id='about'>
-        <h2 className={styles.title}>Acerca de mi</h2>
-        <div className={styles.content}>
-            <img src={getImageUrl("about/yo_programando.png")} alt="Yo programando" className={styles.aboutImage} />
-            <ul className={styles.aboutItems}>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
-                    <div className={styles.aboutItemText}>
-                        <h3>Frontend Developer</h3>
-                        <p>Experiencia en creación de páginas responsive y SEO optimizado.</p>
-                    </div>
-                </li>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
-                    <div className={styles.aboutItemText}>
-                        <h3>Backend developer</h3>
-                        <p>Experiencia en creación de APIs optimizadas con .NET Core y manejo de bases de datos SQL y NoSQL.</p>
-                    </div>
-                </li>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/cursorIcon.png")} alt="UI Icon" />
-                    <div className={styles.aboutItemText}>
-                        <h3>Diseño UI</h3>
-                        <p>Manejo de Figma para diseño de páginas web.</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>Acerca de mi</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/yo_programando.png")}
+          alt="Yo programando"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                Experiencia en creación de páginas responsive, UX/UI y SEO
+                optimizado.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend developer</h3>
+              <p>
+                Experiencia en creación de APIs optimizadas con .NET Core y
+                manejo de bases de datos SQL y NoSQL.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Automatización & AI</h3>
+              <p>
+                Manejo herramientas como n8n para automatizar flujos y conectar
+                herramientas, por medio de técnicas como RAG o MCP para crear
+                modelos que automaticen consultas de fuentes personalizables.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </section>
-  )
-}
+  );
+};
--- hi-im-cristopher/src/components/Hero/Hero.jsx
+++ hi-im-cristopher/src/components/Hero/Hero.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Hero.module.css';
-import { getImageUrl } from '../../utils';
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Hola, soy Cristopher</h1>
-            <p className={styles.description}>Soy un desarrollador full stack con 3 años de experiencia en .NET y SQL Server.</p>
-            <a href="mailto:cristopher.vanegas.21@gmail.com" className={styles.contactBtn}>Contáctame</a>
-        </div>
-        <img src={ getImageUrl("hero/me_hero.jpg") } className={styles.heroImg} alt="Hero Image of me" />
-        <div className={styles.topBlur}></div>
-        <div className={styles.bottomBlur}></div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hola, soy Cristopher</h1>
+        <p className={styles.description}>
+          {
+            " > Desarrollador FullStack con experiencia en sistemas ERP & CRM, apasionado por la IA y la programación de sistemas embebidos."
+          }
+        </p>
+        <a
+          href="mailto:cristopher.vanegas.21@gmail.com"
+          className={styles.contactBtn}
+        >
+          Contáctame
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/hero2.jpeg")}
+        className={styles.heroImg}
+        alt="Hero Image of me"
+      />
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
     </section>
-  )
-}
+  );
+};
