ac2077ec632e9d4134616e311110593f19b65b18
Cristopher Vanegas <cristopher.platzi@gmail.com>
2026-05-01 23:50:51 -0500

    ADD: Added state to open images when clicked

--- hi-im-cristopher/src/components/Projects/ProjectCard/ProjectCard.jsx
+++ hi-im-cristopher/src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import styles from './ProjectCard.module.css';
 
 import { getImageUrl } from '../../../utils';
 
-export const ProjectCard = ({ 
-    project : { title, imageSrc, description, skills, demo, source } 
+export const ProjectCard = ({
+    project: { title, imageSrc, description, skills, demo, source }
 }) => {
-  return (
+    const [isImageOpen, setIsImageOpen] = useState(false);
+    return (
         <div className={styles.container}>
-            <img src={getImageUrl(imageSrc)} alt={`Imagen de ${title}`} className={styles.image} />
+            {isImageOpen && (
+                <div className={styles.imageModal} onClick={() => setIsImageOpen(false)}>
+                    <img
+                        src={getImageUrl(imageSrc)}
+                        alt={`Imagen ampliada de ${title}`}
+                        className={styles.imageModalContent}
+                    />
+                </div>
+            )}
+            {/* <img src={getImageUrl(imageSrc)} alt={`Imagen de ${title}`} className={styles.image} /> */}
+            <img
+                src={getImageUrl(imageSrc)}
+                alt={`Imagen de ${title}`}
+                className={styles.image}
+                onClick={() => setIsImageOpen(true)}
+            />
             <h3 className={styles.title}>{title}</h3>
             <p className={styles.description}>{description}</p>
             <ul className={styles.skills}>
@@ -23,8 +38,8 @@ export const ProjectCard = ({
             </ul>
             <div className={styles.links}>
                 <a href={demo} className={styles.link}>Demo</a>
-                <a href={source} className={styles.link }>Source</a>
+                <a href={source} className={styles.link}>Source</a>
             </div>
-        </div>  
-        )
+        </div>
+    )
 }
